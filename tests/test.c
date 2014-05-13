ec3ddcdecb9e232841c7930da7a4f5d6127b3af5
Jordan Horwich <jordan@voidingwarranties.com>
2014-05-13 15:25:02 -0400

    Add more tests and update makefile

--- tests/test.c
+++ tests/test.c
@@ -41,7 +41,26 @@ void test_andi(union mips_instruction inst, struct virtual_mem_region* memory, s
 	assert(ctx.regs[s3] == UINT16_MAX);	// andi s3, t0, UINT16_MAX
 	assert(ctx.regs[s4] == 1);			// andi s4, t0, 1
 	assert(ctx.regs[s5] == 5);			// andi s5, t0, 5
+	assert(ctx.regs[s6] == 0);			// andi s6, zero, 0
 	printf("#################### andi tests finished  ######################################\n");
+}
+
+void test_ori(union mips_instruction inst, struct virtual_mem_region* memory, struct context ctx)
+{
+	printf("#################### ori tests start ##########################################\n");
+	ReadELF("ori.elf", &memory, &ctx);
+	RunSimulator(memory, &ctx);
+
+	assert(ctx.regs[s0] == UINT16_MAX);	// ori s0, zero, UINT16_MAX
+	assert(ctx.regs[s1] == 1);			// ori s1, zero, 1
+	                          			// addiu t0, zero, UINT16_MAX
+	assert(ctx.regs[s3] == UINT32_MAX);	// ori s3, t0, UINT16_MAX
+	assert(ctx.regs[s4] == UINT32_MAX);	// ori s4, t0, 1
+	                          			// addiu t0, zero, 8
+	assert(ctx.regs[s5] == 9);			// ori s5, t0, 1
+	assert(ctx.regs[s6] == 24);			// ori s6, t0, 16
+	assert(ctx.regs[s7] == 8);			// ori s7, t0, 8
+	printf("#################### ori tests finished  ######################################\n");
 }
 
 int main()
@@ -53,6 +72,7 @@ int main()
 	test_addi(inst, memory, ctx);
 	test_addiu(inst, memory, ctx);
 	test_andi(inst, memory, ctx);
+	test_ori(inst, memory, ctx);
 
 	printf("All tests completed successfully!\n");
 	return 0;
